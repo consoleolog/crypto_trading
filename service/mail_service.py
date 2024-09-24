4c05720423adffd78b9ff383cb1e921c1d255acb
consolelog <leopportunity5@gmail.com>
2024-09-24 17:01:56 +0900

    2024-09-24 :: - 로그 파일 보내는 함수 추가

--- service/mail_service.py
+++ service/mail_service.py
@@ -1,6 +1,8 @@
+from email.mime.text import MIMEText
+
 from config import *
 
-def send_mail():
+def send_mail(content):
     import os
     import smtplib
     from email import encoders
@@ -10,8 +12,35 @@ def send_mail():
     msg['Subject'] = '[Upbit] Trading Result files'
     msg['From'] = SMTP_FROM
     msg['To'] = SMTP_TO
+    part = MIMEText(f"<h4>{content}</h4>", 'html')
+    msg.attach(part)
     for filename in os.listdir('./data'):
         with open(f'./data/{filename}', 'rb') as f:
+            file = MIMEBase("application", "octet-stream")
+            file.set_payload(f.read())
+        encoders.encode_base64(file)
+        file.add_header("Content-Disposition", f"attachment; filename= {filename}")
+        msg.attach(file)
+    s = smtplib.SMTP('smtp.naver.com', 587)
+    s.starttls()
+    s.login(NAVER_ID, NAVER_PASSWORD)
+    s.sendmail(SMTP_FROM, SMTP_TO, msg.as_string())
+    s.close()
+
+def send_log_file(content):
+    import os
+    import smtplib
+    from email import encoders
+    from email.mime.base import MIMEBase
+    from email.mime.multipart import MIMEMultipart
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = '[Upbit] Trading Result files'
+    msg['From'] = SMTP_FROM
+    msg['To'] = SMTP_TO
+    part = MIMEText(f"<h4>{content}</h4>", 'html')
+    msg.attach(part)
+    for filename in os.listdir('./logs'):
+        with open(f'./logs/{filename}', 'rb') as f:
             file = MIMEBase("application", "octet-stream")
             file.set_payload(f.read())
         encoders.encode_base64(file)
