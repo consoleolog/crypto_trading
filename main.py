63f0da44f4ec6bb3ddd97b59c54ea3e01b1e4306
consolelog <leopportunity5@gmail.com>
2024-09-24 17:32:44 +0900

    2024-09-24 :: - 스테이지별 실행 함수 수정

--- main.py
+++ main.py
@@ -54,7 +54,6 @@ def main(df):
 
 while True:
 
-
     try :
         df = get_ema_data(ticker=ticker, interval="minute5",count=240)
 
@@ -62,33 +61,15 @@ while True:
 
         log.info(stage)
 
-        if stage == "stage6":
-            log.info("매수 구간 돌입")
-            main(df)
-
-        if stage == "stage1":
-            log.info("매도 구간 돌입")
-            main(df)
-
-        if stage == "stage4" or stage == "stage5" and get_macd_gradient_for_sell(df) == "BUY_TRUE":
+        if (stage == "stage4" or stage == "stage5" or stage == "stage6") and get_macd_gradient_for_sell(df) == "BUY_TRUE":
             log.info("매수 신호")
-            amount = get_balances(ticker)
-            b_result = do_buy(ticker, amount, 6000)
-            if b_result != "ALREADY_BUY":
-                send_mail("매수 결과")
+            main(df)
 
-        if stage == "stage2" or stage == "stage3" and get_macd_gradient_for_sell(df) == "SELL_TRUE":
+        if (stage == "stage1" or stage == "stage2" or stage == "stage3") and get_macd_gradient_for_sell(df) == "SELL_TRUE":
             log.info("매도 신호")
-            amount = get_balances(ticker)
-            s_result = do_sell(ticker, amount)
-            if s_result != "ALREADY_SELL":
-                send_mail("매도 결과")
+            main(df)
 
-        # 1시간마다 send_mail() 호출
         current_time = datetime.now()
-        # if current_time - last_send_time >= timedelta(hours=1):
-        #     send_mail()
-        #     last_send_time = current_time  # 마지막으로 메일을 보낸 시간을 업데이트
 
         # 12시간마다 로그 파일 , 데이터 파일 초기화
         if current_time - last_send_time >= timedelta(hours=3):
@@ -116,4 +97,4 @@ while True:
         log.error(e)
         pass
 
-    time.sleep(180)
+    time.sleep(100)
