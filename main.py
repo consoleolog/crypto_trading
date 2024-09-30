de88fc94cb8ea7b836643dbb95eca9066ee10459
consoleolog <leopportunity5@gmail.com>
2024-09-30 20:03:28 +0900

    2024-09-30 :: - main 함수 수정

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import time
 import schedule
 
 from app import App
-from service.aws_service import AWSService
+# from service.aws_service import AWSService
 
 from service.mail_service import MailService
 
 schedule.every(5).seconds.do(App)
 
 mail_service = MailService()
-aws_service = AWSService()
+# aws_service = AWSService()
 schedule.every(1).hours.do(mail_service.send_mail, "데이터 백업")
 schedule.every(1).hours.do(mail_service.send_log_file, "로그 파일 백업")
-schedule.every(1).hours.do(aws_service.upload_data_files)
+# schedule.every(1).hours.do(aws_service.upload_data_files)
 
 if __name__ == '__main__':
     while True:
