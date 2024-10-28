f338c33bcfd5117e3b4bd8a3e95cfbac90e1ac0a
consolelog <leopportunity5@gmail.com>
2024-10-28 16:05:26 +0900

    2024-10-28 :: - logger 로 다시 분리

--- model/trade.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-class Trade:
-    def __init__(self, data: dict[str, Any]):
-        self.create_time = data["created_at"]
-        self.ticker = data["market"]
-        self.price = data["locked"]
-        self.market_price = data["market_price"]
