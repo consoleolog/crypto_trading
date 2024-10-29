3852f9a62af820b63f336c51ba05fc8a987518f9
consolelog <leopportunity5@gmail.com>
2024-10-29 10:22:31 +0900

    2024-10-29 :: - logger 수정

--- logger.py
+++ logger.py
@@ -1,19 +1,41 @@
 import logging
 import os
 
-fmt = "[%(levelname)s] %(asctime)s : %(filename)s : %(module)s : %(lineno)d - %(message)s"
+def get_logger(logging_service):
+    fmt = f"[%(levelname)s] %(asctime)s : %(filename)s : %(module)s : %(lineno)d :: {logging_service} :: -  %(message)s"
 
-datefmt = '%Y-%m-%d %H:%M:%S'
+    datefmt = '%Y-%m-%d %H:%M:%S'
 
-filefmt = '%Y%m%d%H%M'
+    class CustomFormatter(logging.Formatter):
+        grey = '\x1b[38;21m'
+        blue = '\x1b[38;5;39m'
+        yellow = '\x1b[38;5;226m'
+        red = '\x1b[38;5;196m'
+        bold_red = '\x1b[31;1m'
+        reset = '\x1b[0m'
+        green = '\x1b[38;5;46m'
+        bold_green = '\x1b[32;1m'
 
-def get_logger(logging_service:str ="undefined"):
+        def __init__(self):
+            super().__init__()
+            self.fmt = fmt
+            self.FORMATS = {
+                logging.DEBUG: self.bold_green + self.fmt + self.reset,
+                logging.INFO: self.blue + self.fmt + self.reset,
+                logging.WARNING: self.yellow + self.fmt + self.reset,
+                logging.ERROR: self.red + self.fmt + self.reset,
+                logging.CRITICAL: self.bold_red + self.fmt + self.reset
+            }
+
+        def format(self, record):
+            log_fmt = self.FORMATS.get(record.levelno)
+            formatter = logging.Formatter(log_fmt, datefmt=datefmt)
+            return formatter.format(record)
+
     logger = logging.getLogger(f"{logging_service}_logger")
     logger.setLevel(logging.DEBUG)
     logger.propagate = False
     log_dir = f"{os.getcwd()}/logs"
-    if not os.path.exists(log_dir):
-        os.mkdir(log_dir)
 
     file_formatter = logging.Formatter(
         fmt=fmt,
@@ -29,30 +51,3 @@ def get_logger(logging_service:str ="undefined"):
     logger.addHandler(ch)
 
     return logger
-
-
-class CustomFormatter(logging.Formatter):
-    grey = '\x1b[38;21m'
-    blue = '\x1b[38;5;39m'
-    yellow = '\x1b[38;5;226m'
-    red = '\x1b[38;5;196m'
-    bold_red = '\x1b[31;1m'
-    reset = '\x1b[0m'
-    green = '\x1b[38;5;46m'
-    bold_green = '\x1b[32;1m'
-
-    def __init__(self):
-        super().__init__()
-        self.fmt = fmt
-        self.FORMATS = {
-            logging.DEBUG: self.bold_green + self.fmt + self.reset,
-            logging.INFO: self.blue + self.fmt + self.reset,
-            logging.WARNING: self.yellow + self.fmt + self.reset,
-            logging.ERROR: self.red + self.fmt + self.reset,
-            logging.CRITICAL: self.bold_red + self.fmt + self.reset
-        }
-
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt, datefmt=datefmt)
-        return formatter.format(record)
